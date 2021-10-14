852e9d745b7256fc98f701f118c17233058d268e
PATH-68 <pierre-antoine.thiam@hotmail.com>
2021-10-14 17:20:11 +0000

    sign in still in progress

--- lib/screens/Connexion/signin.dart
+++ lib/screens/Connexion/signin.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:task_app/main.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+final GoogleSignIn gSignIn = GoogleSignIn();
 
 class Signin extends StatefulWidget {
  Signin({Key? key}) : super(key: key);
@@ -12,6 +15,39 @@ class Signin extends StatefulWidget {
 class  SigninState extends State <Signin> {
 
 bool isSignedIn = false;
+
+void initState() {
+  super.initState();
+
+  gSignIn.onCurrentUserChanged.listen((gSigninAccount) {
+    ControlSignIn(gSigninAccount!);
+   }, onError: (gError) {
+     print('Not sign in' + gError.message);
+   });
+
+   gSignIn.signInSilently(suppressErrors: false).then((gSignInAccount) {
+     ControlSignIn(gSignInAccount!);
+     } ).catchError;
+}
+
+ControlSignIn(GoogleSignInAccount signInAccount) async {
+  if (signInAccount != null) {
+     setState(() {
+       isSignedIn = true;
+     });
+  }
+  else {
+    isSignedIn = false;
+  }
+}
+
+loginUser() {
+gSignIn.signIn();
+}
+
+logOutUser() {
+  gSignIn.signOut();
+}
 
 Widget buildScreen() {
   return Text('Already signed in');
@@ -28,7 +64,7 @@ Scaffold buildSignInScreen() {
           Text('Welcome to Tasky !', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 45, fontFamily: "Signatra")),
           SizedBox(height: 30),
           GestureDetector(
-            onTap: () => "Toc Toc",
+            onTap: () => loginUser,
             child: Container(
               width: 270,
               height: 55,
