1ac0267996ca64c80700dd3fbc7713bd6f0ab4e1
PATH-68 <pierre-antoine.thiam@hotmail.com>
2021-10-19 14:42:34 +0000

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/services.dart';
 import 'package:task_app/screens/home/home.dart';
 import 'package:task_app/screens/home/widgets/tasks.dart';
 import 'package:task_app/screens/stats/stats.dart';
-import 'package:task_app/screens/Friends/friends.dart';
+import 'package:task_app/screens/localisation/localisation.dart';
 import 'package:task_app/screens/my_tasks/my_tasks.dart';
 import 'package:task_app/screens/Actus/actus.dart';
 import 'package:task_app/screens/Connexion/signin.dart';
--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -71,7 +71,7 @@ class _HomePageState extends State<HomePage> {
           //           fit: BoxFit.contain,
           //         ),
           //       ),
-      
+          
       : Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
